abdecb7496a1f4baf0c8b350d877ba13c33ffcea
Milan Shrestha <shrestham780@gmail.com>
2023-03-06 13:32:00 +0545

    app

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(WhatApp());
+}
+
+class WhatApp extends StatefulWidget {
+  const WhatApp({super.key});
+
+  @override
+  State<WhatApp> createState() => _WhatAppState();
+}
+
+class _WhatAppState extends State<WhatApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
